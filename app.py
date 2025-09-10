17942ce717d2e7c0838dad12e099b647eda183f9
Rajalaxmi Parida <prajalaxmi737@gmail.com>
2025-09-10 22:19:32 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,32 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask, request, render_template
 app = Flask(__name__, template_folder='template')
-stations = {
-    "Bhubaneswar": 0,
-    "Cuttack": 25,
-    "Puri": 60,
-    "Balasore": 150,
-    "Berhampur": 180
-}
-price_per_km = 1  
-seat_number = 1 
-@app.route('/')
-def home():
-    return render_template("index.html", stations=stations.keys())
-@app.route('/calculate', methods=['POST'])
-def calculate():
-    source = request.form['source']
-    destination = request.form['destination']
 
-    if source == destination:
-        return render_template("index.html", stations=stations.keys(), 
-                               message="Source and Destination cannot be same!")
-    distance = abs(stations[destination] - stations[source])
-    cost = distance * price_per_km
+@app.route('/', methods=['GET', 'POST'])
+def weather_data():
+    if request.method == 'POST':
+        humidity = request.form.get('humidity')
+        temperature = request.form.get('temperature')
+        rainfall = request.form.get('rainfall')
+        return render_template('results.html', humidity=humidity, temperature=temperature, rainfall=rainfall)
+    return render_template('index.html')
 
-    return render_template("index.html", stations=stations.keys(),
-                           cost=cost,
-                           source=source,
-                           destination=destination,
-                           seat=f"S-{seat_number}")
 if __name__ == '__main__':
     app.run(debug=True)
