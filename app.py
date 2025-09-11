0aa26ac7a70ba2aa717ac847fc80fe7719177c76
Rajalaxmi Parida <prajalaxmi737@gmail.com>
2025-09-11 09:25:49 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__, template_folder='template')
 
-@app.route('/', methods=['GET', 'POST'])
-def weather_data():
-    if request.method == 'POST':
-        humidity = request.form.get('humidity')
-        temperature = request.form.get('temperature')
-        rainfall = request.form.get('rainfall')
-        return render_template('results.html', humidity=humidity, temperature=temperature, rainfall=rainfall)
+@app.route('/')
+def index():
     return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        size = float(request.form['size'])
+        bedrooms = int(request.form['bedrooms'])
+        location = int(request.form['location'])
+        location_bonus = {1: 500000, 2: 300000, 3: 100000}
+        price = (size * 1500) + (bedrooms * 10000) + location_bonus.get(location, 0)
+
+        return render_template('index.html', prediction=round(price, 2))
+
+    except Exception as e:
+        return render_template('index.html', error=str(e))
 
 if __name__ == '__main__':
     app.run(debug=True)
